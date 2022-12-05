9349ba8c7851f90a9cb14d0979963ff55769dc17
kawuka <kawuka@kawuka.xyz>
2022-12-05 18:28:09 +0100

    try to get https local

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
+import basicSsl from '@vitejs/plugin-basic-ssl'
+import mkcert from 'vite-plugin-mkcert'
 
 export default defineConfig({
     plugins: [
@@ -9,5 +11,9 @@ export default defineConfig({
             refresh: true,
         }),
         react(),
+        mkcert()
     ],
+    server: {
+        https: true,
+    }
 });
