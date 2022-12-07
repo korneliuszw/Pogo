bc44bc04092cb591c0862b1cfdb39bd88b7c17df
kawuka <kawuka@kawuka.xyz>
2022-12-07 21:14:54 +0100

    New notification query

--- app/Schedules/NotificationSender.php
+++ app/Schedules/NotificationSender.php
@@ -9,10 +9,12 @@ use Illuminate\Support\Carbon;
 use Minishlink\WebPush\WebPush;
 use Minishlink\WebPush\Subscription;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\DB;
+
 
 class NotificationSender {
     private WebPush $push;
-
+    private int $maxBatchSize = 600;
     public function __construct() {
         $this->push = new WebPush([
             'VAPID' => [
@@ -23,12 +25,27 @@ class NotificationSender {
         ], [
             'TTL' => 600,
             'urgency' => 'high',
+            'batchSize' => $this->maxBatchSize
         ]);
     }
-
+    private function flush() {
+        foreach ($this->push->flush() as $p) {
+            // let go
+        }
+    }
     public function invoke() {
-        $notifications = Notification::all();
-        foreach ($notifications as $notification) {
+        $notify = DB::table('notifications')
+            ->join('tasks', 'notifications.user_id', '=', 'tasks.user_id')
+            ->whereRaw('tasks.scheduled_at <= CURRENT_TIMESTAMP')
+            ->where('notification_sent' ,false)
+            ->select('tasks.id', 'tasks.task', 'notifications.endpoint',
+                'notifications.p256dh', 'notifications.auth', 'tasks.scheduled_at')
+            ->get();
+        $idx = 0;
+        dump($notify);
+        foreach($notify as $notification) {
+            if ($idx >= $this->maxBatchSize)
+                $this->flush();
             $subscription = Subscription::create([
                 'endpoint' => $notification->endpoint,
                 'contentEncoding' => 'aesgcm',
@@ -39,22 +56,49 @@ class NotificationSender {
                     'p256dh' => $notification->p256dh
                 ]
             ]);
-            $users = $notification->belongsTo(User::class, 'user_id')->get();
-            foreach ($users as $user) {
-                foreach ($user->tasks()->incomplete()->whereRaw('scheduled_at <= CURRENT_TIMESTAMP')->where('notification_sent', false)->get() as $task) {
-                    $task_short = substr($task->task, 0, 50);
-                    $this->push->queueNotification($subscription, json_encode([
-                        "msg_up" => "Przypomnienie",
-                        "msg_down" => "Zadanie $task_short zostalo rozpoczete",
-                        "timestamp" => $task->scheduled_at
-                    ]));
-                    $task->update(['notification_sent' => true]);
-                }
-            }
+            $task_short = substr($notification->task, 0, 50);
+            $this->push->queueNotification($subscription, json_encode([
+                "msg_up" => "Przypomnienie",
+                "msg_down" => "Zadanie $task_short zostalo rozpoczete",
+                "timestamp" => $notification->scheduled_at
+            ]));
+            DB::table('tasks')
+                ->where('id', $notification->id)
+                ->update(['notification_sent' => true]);
+            $idx+=1;
         }
-        foreach ($this->push->flush() as $p) {
-            // let generator go
-        }
+        $this->flush();
     }
+
+    // public function invoke() {
+    //     $notifications = Notification::all();
+    //     foreach ($notifications as $notification) {
+    //         $subscription = Subscription::create([
+    //             'endpoint' => $notification->endpoint,
+    //             'contentEncoding' => 'aesgcm',
+    //             'publicKey' => $notification->p256dh,
+    //             'authToken' => $notification->auth,
+    //             'keys' =>  [
+    //                 'auth' => $notification->auth,
+    //                 'p256dh' => $notification->p256dh
+    //             ]
+    //         ]);
+    //         $users = $notification->belongsTo(User::class, 'user_id')->get();
+    //         foreach ($users as $user) {
+    //             foreach ($user->tasks()->incomplete()->whereRaw('scheduled_at <= CURRENT_TIMESTAMP')->where('notification_sent', false)->get() as $task) {
+    //                 $task_short = substr($task->task, 0, 50);
+    //                 $this->push->queueNotification($subscription, json_encode([
+    //                     "msg_up" => "Przypomnienie",
+    //                     "msg_down" => "Zadanie $task_short zostalo rozpoczete",
+    //                     "timestamp" => $task->scheduled_at
+    //                 ]));
+    //                 $task->update(['notification_sent' => true]);
+    //             }
+    //         }
+    //     }
+    //     foreach ($this->push->flush() as $p) {
+    //         // let generator go
+    //     }
+    // }
 }
 ?>
